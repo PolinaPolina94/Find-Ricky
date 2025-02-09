c62a40637d40f4aac8513e34461e58cf100b86a4
Полина <polina_kazhina@mail.ru>
2025-02-09 18:48:13 +0300

    clear main branch

--- src/servises/convertDate.ts
+++
@@ -1,5 +0,0 @@
-export const convertDate = (str: string) => {
-    const date = new Date(str);
-    const convertedDate = date.toLocaleDateString('en-GB').replace(/\//g, '.');
-    return convertedDate;
-}
--- src/servises/links.ts
+++
@@ -1,4 +0,0 @@
-export const API_LINK = 'https://rickandmortyapi.com/api/';
-export const LND = 'https://www.linkedin.com/in/kazhynapalina';
-export const CV = 'https://drive.google.com/file/d/1tRxaYoBkSBXgNFHH7OcA13iY30yFOLXP/view';
-export const TLG = 'https://t.me/laralaree';
